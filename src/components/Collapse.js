ed923c2217edb3f1061768eebf31254b26e6a606
emir <mahmutemirkr@gmail.com>
2021-05-20 16:18:38 +0300

    react state

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 
-const Collapse = (props) => {
+class Collapse extends React.Component {
+    render() {
+        return(
+        <div>
+            <a class="btn btn-primary w-100 mt-3 mb-2" data-toggle="collapse" href={"#".concat(this.props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
+                Collapse Card
+            </a>
+            <div class="collapse show" id={this.props.href}>
+                {this.props.children}
+            </div>
+        </div>
+        );
+    }
+}
+
+
+
+
+/* const Collapse = (props) => {
     console.log(props.children);
     return (
         <div>
@@ -13,6 +31,6 @@ const Collapse = (props) => {
             </div>
         </div>
     );
-}
+} */
 
 export default Collapse;
