6626ca40b6a2a89ff40518fc8c7569010441289d
emir <mahmutemirkr@gmail.com>
2021-05-23 16:24:52 +0300

    setState Update

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -35,20 +35,7 @@ class Collapse extends React.Component {
     }
 }
 
-
 export default Collapse;
 
 
-/* const Collapse = (props) => {
-    console.log(props.children);
-    return (
-        <div>
-            <a class="btn btn-primary w-100 mt-3 mb-2" data-toggle="collapse" href={"#".concat(props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
-               Card Open
-            </a>
-            <div class="collapse show" id={props.href}>
-                {props.children}
-            </div>
-        </div>
-    );
-} */
+
